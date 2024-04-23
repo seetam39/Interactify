4687e6583e57524de0c55b14ed0e5578c9c44143
Seetam <123747653+seetam39@users.noreply.github.com>
2024-04-23 20:49:49 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://interactify-6.onrender.com", {
+			const socket = io("https://interactify-web-app.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
